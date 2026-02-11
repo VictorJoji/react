443122c1db35371264813befaf7fb91f791552ab
VictorJoji <victorurieljojidemacedo@gmail.com>
2026-02-11 16:37:41 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export  function App() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <p>Entre com seu e-mail e senha para acessar o sistema.</p>
+      <input type="email" placeholder="Seu e-mail" />
+      <input type="password" placeholder="Sua senha" />
+      <button>Entrar</button>
+    </main>
+  );
+}
